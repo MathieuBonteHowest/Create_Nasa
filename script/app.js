cb6006ff41935ae40668508189c717f9ba0fcb37
Mathieu Bonte <mathieu.bonte@student.howest.be>
2017-12-08 13:03:00 +0100

    afgewerkte site

--- script/app.js
+++ script/app.js
@@ -5,101 +5,42 @@ document.addEventListener( 'DOMContentLoaded', function() {
     console.info( 'The document is loaded and the file is linked to the index.html file.');
 });
 
+var params = {
+    container: document.getElementById('lottie'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'data.json'
+};
 
+var anim;
+anim = lottie.loadAnimation(params);
 
 var key = "nQGhQm8MtOQ4mTrszAnbt0NbFBZbZbYWmIJRgPxU";
 
-// var date = "2017-12-1";
+var x = 2;
 
-var today = new Date();
-var todayFormat = today.toISOString().substring(0, 10);
-var todayDay = todayFormat.substring(8,10);
-var todayMonth = todayFormat.substring(5,7);
-var todayYear = todayFormat.substring(0,4);
-var prevDayGlobal = todayDay;
-var prevMonthGlobal = todayMonth;
-var prevYearGlobal = todayYear;
+var today = new Date.today();
+showPicture(today);
+
+var dateGlobal = today;
 
 $('#prev').on('click', function(){
-    var prevDay = prevDayGlobal - 1;
-    var prevMonth = prevMonthGlobal;
-    var prevYear = prevYearGlobal;
-
-    if(prevDay == 0){
-        prevMonth = prevMonthGlobal - 1;
-        prevDay = 30;
-    }
-    if (prevMonth == 0){
-        prevYear = prevYearGlobal - 1;
-        prevMonth = 12;
-    }
-    url = "https://api.nasa.gov/planetary/apod?api_key="+key+"&date="+prevYear+"-"+prevMonth+"-"+prevDay;
-    $.ajax({
-        url: url,
-        success: function(result){
-            if("copyright" in result) {
-                $("#copyright").text("Image Credits: " + result.copyright);
-            }
-            else {
-                $("#copyright").text("Image Credits: " + "Public Domain");
-            }
-
-            $("#apod__img__id").css("display", "none");
-            $("#apod__vid__id").css("display", "none");
-            $(".video__container").css("display", "none");
-            $(".container").css("display", "none");
-
-            if(result.media_type == "video") {
-                $("#apod__vid__id").css("display", "block");
-                $(".video__container").css("display", "block");
-                $("#apod__vid__id").attr("src", result.url);
-                $(".video__container").css("height", "600px");
-                $('.video__container').addClass('is-visible');
-            }
-            else {
-                $("#apod__img__id").css("display", "block");
-                $(".container").css("display", "block");
-                $(".video__container").css("height", "0");
-                $("#apod__img__id").attr("src", result.url);
-                $('.container').addClass('is-visible');
-            }
-
-            console.info(JSON.stringify(result, null, 4));
-            $("#apod__explaination").text(result.explanation);
-            $("#apod__title").text(result.title);
-            $("#apod__title__mainpage").text(result.title);
-            $("#apod__date").text(result.date);
-
-            if(result.date == todayFormat){
-                $("#next").css("display", "none");
-            }
-            else{
-                $("#next").css("display", "inline-block");
-            }
-            // setTimeout(loader,3000);
-
-        }
-    });
-    prevDayGlobal = prevDay;
-    prevMonthGlobal = prevMonth;
-    prevYearGlobal = prevYear;
+    var prev = (x).days().ago();
+    console.log(prev);
+    showPicture(prev);
+    x = x + 1;
 });
 
 $('#next').on('click', function(){
-    var nextDay = prevDayGlobal + 1;
-    var nextMonth = prevMonthGlobal;
-    var nextYear = prevYearGlobal;
+    var next = dateGlobal.add(1).day();
+    showPicture(next);
+});
 
-
-    if(nextDay == 31){
-        nextMonth = prevMonthGlobal + 1;
-        nextDay= 1;
-    }
-    if (nextMonth == 12 && nextDay == 30){
-        nextYear = prevYearGlobal + 1;
-        nextMonth = 1;
-    }
-    url = "https://api.nasa.gov/planetary/apod?api_key="+key+"&date="+nextYear+"-"+nextMonth+"-"+nextDay;
+// ---------- TONEN VAN DE AFBEELDING ----------
+function showPicture(value) {
+    var valueSlice = value.toISOString().slice(0,10);
+    var url = "https://api.nasa.gov/planetary/apod?api_key="+key+"&date="+valueSlice;
     $.ajax({
         url: url,
         success: function(result){
@@ -110,106 +51,48 @@ $('#next').on('click', function(){
                 $("#copyright").text("Image Credits: " + "Public Domain");
             }
 
-            $("#apod__img__id").css("display", "none");
             $("#apod__vid__id").css("display", "none");
-            $(".video__container").css("display", "none");
+            $("#apod__img__id").css("display", "none");
             $(".container").css("display", "none");
+            $(".video__container").css("display", "none");
+
 
             if(result.media_type == "video") {
-                $("#apod__vid__id").css("display", "block");
                 $(".video__container").css("display", "block");
+                $("#apod__vid__id").css("display", "block");
                 $("#apod__vid__id").attr("src", result.url);
                 $(".video__container").css("height", "600px");
                 $('.video__container').addClass('is-visible');
             }
             else {
-                $("#apod__img__id").css("display", "block");
                 $(".container").css("display", "block");
-                $(".video__container").css("height", "0");
+                $("#apod__img__id").css("display", "block");
                 $("#apod__img__id").attr("src", result.url);
                 $('.container').addClass('is-visible');
             }
 
-            console.info(JSON.stringify(result, null, 4));
+            // console.info(JSON.stringify(result, null, 4));
             $("#apod__explaination").text(result.explanation);
             $("#apod__title").text(result.title);
             $("#apod__title__mainpage").text(result.title);
             $("#apod__date").text(result.date);
 
-            if(result.date == todayFormat){
+            if(result.date == today.toISOString().slice(0,10)){
                 $("#next").css("display", "none");
             }
             else{
                 $("#next").css("display", "inline-block");
             }
-            // setTimeout(loader,3000);
+
+            setTimeout(loader,3000);
 
         }
+
     });
-    prevDayGlobal = nextDay;
-    prevMonthGlobal = nextMonth;
-    prevYearGlobal = nextYear;
-});
+    dateGlobal = value;
+}
 
-var url = "https://api.nasa.gov/planetary/apod?api_key="+key+"&date="+todayYear+"-"+todayMonth+"-"+todayDay;
-
-// var params = {
-//     container: document.getElementById('lottie'),
-//     renderer: 'svg',
-//     loop: true,
-//     autoplay: true,
-//     path: 'data.json'
-// };
-//
-// var anim;
-//
-// anim = lottie.loadAnimation(params);
-
-$.ajax({
-    url: url,
-    success: function(result){
-        if("copyright" in result) {
-            $("#copyright").text("Image Credits: " + result.copyright);
-        }
-        else {
-            $("#copyright").text("Image Credits: " + "Public Domain");
-        }
-
-        $("#apod__vid__id").css("display", "none");
-        $("#apod__img__id").css("display", "none");
-        $(".container").css("display", "none");
-        $(".video__container").css("display", "none");
-
-
-        if(result.media_type == "video") {
-            $(".video__container").css("display", "block");
-            $("#apod__vid__id").css("display", "block");
-            $("#apod__vid__id").attr("src", result.url);
-            $(".video__container").css("height", "600px");
-            $('.video__container').addClass('is-visible');
-        }
-        else {
-            $(".container").css("display", "block");
-            $("#apod__img__id").css("display", "block");
-            $("#apod__img__id").attr("src", result.url);
-            $('.container').addClass('is-visible');
-        }
-
-        // console.info(JSON.stringify(result, null, 4));
-        $("#apod__explaination").text(result.explanation);
-        $("#apod__title").text(result.title);
-        $("#apod__title__mainpage").text(result.title);
-        $("#apod__date").text(result.date);
-
-        if(result.date == todayFormat){
-            $("#next").css("display", "none");
-        }
-
-        // setTimeout(loader,3000);
-
-    }
-});
-
+// ---------- POPUP ----------
 jQuery(document).ready(function($){
     //open popup
     $('#apod__img__id').on('click', function(event){
@@ -232,11 +115,12 @@ jQuery(document).ready(function($){
     });
 });
 
+// ---------- LOADING EFFECT ----------
 function loader() {
     $("#lottie").css("display", "none");
 }
 
-
+// ---------- TOGGLE FUNCTION ----------
 function nightMode(x, _this) {
     if (_this.checked) {
         x.style.backgroundColor = '#222222';
@@ -246,6 +130,7 @@ function nightMode(x, _this) {
         document.getElementById("apod__date").style.color = '#fdfefe';
         document.getElementById("apod__date").style.color = '#fdfefe';
         document.getElementById("nightMode").style.color = '#fdfefe';
+
 
 
     } else  {
